8c3f8d864347253173686f4b4f6980c5d60fee88
sashreekkrispy <sashreek.uyyala@gmail.com>
2023-09-12 18:15:06 +0530

    Revert "sadas"
    This reverts commit c6a175be3f4ba8de6743ce97f427acf02a94e8e8.

--- volvo_day_car_simvr/Assets/Shreehith assets/prefabs1/scripts/scripts/spawnAtTip (1).cs
+++ volvo_day_car_simvr/Assets/Shreehith assets/prefabs1/scripts/scripts/spawnAtTip (1).cs
@@ -1,51 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class spawnAtTip : MonoBehaviour
 {
-    public GameObject prefabToSpawn;
+    public GameObject prefabToSpawn;                    //first set
     public GameObject parentPrefab;
     private Vector3 banKai;
+    
+    Vector3 mousePosition;
 
+                                                      //second set
     GameObject snapparent;
     Vector3 offset;
 
-    private bool triggerPressed = false; // Track the trigger button state
-
+    // Start is called before the first frame update
     void Start()
     {
-        // Initialize Input System for VR
-        InputDevice device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand); // Change XRNode to LeftHand if needed
-        device.TryGetFeatureValue(CommonUsages.triggerButton, out triggerPressed);
+        
     }
 
+    
+    
+
+    // Update is called once per frame
     void Update()
     {
-        // Check if the trigger button is pressed
-        if (triggerPressed)
+        banKai = this.gameObject.transform.GetChild(1).transform.position;
+        if (Input.GetMouseButtonDown(0))
         {
-            banKai = this.gameObject.transform.GetChild(1).transform.position;
-            Instantiate(prefabToSpawn, banKai, Quaternion.identity, parentPrefab.transform);
+            Instantiate(prefabToSpawn, banKai, Quaternion.identity ,parentPrefab.transform);
+            
         }
     }
 
     void OnTriggerStay(Collider collider)
     {
+
         if (collider.gameObject.tag == "plate" || collider.gameObject.tag == "cuts5" || collider.gameObject.tag == "cuts6" || collider.gameObject.tag == "cuts7" || collider.gameObject.tag == "cuts8")
         {
+            
             snapparent = collider.gameObject;
             offset = transform.position - snapparent.transform.position;
+            
         }
-
         if (collider.gameObject.tag == "Test")
         {
-            if (triggerPressed)
-            {
-                Instantiate(prefabToSpawn, banKai, Quaternion.identity);
-                Debug.Log("lol");
-            }
+            Instantiate(prefabToSpawn, banKai, Quaternion.identity);
+            Debug.Log("lol");
         }
     }
+    
+    
 }
